99d7bd5a9f58b535b9e2479544d667e90315c891
Tanmay <shahtany99@gmail.com>
2017-01-24 23:07:01 -0500

    Nv charts completion

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -58,31 +58,106 @@ angular.module('stock.controllers', [])
   ];
 }])
 
-.controller('StockCtrl',['$scope','$stateParams','stockDataService',
- function($scope, $stateParams, stockDataService) {
-
+.controller('StockCtrl',['$scope','$stateParams','$window','stockDataService','dateService','chartDataService',
+ function($scope, $stateParams,$window,stockDataService,dateService,chartDataService) {
   $scope.ticker=$stateParams.stockTicker;
-  $scope.chartView=1;
+  $scope.oneYearAgoDate = dateService.oneYearAgoDate();
+  $scope.todayDate = dateService.currentDate();
+
+  $scope.chartView=4;
   $scope.$on("$ionicView.afterEnter", function(){
       getPrice();
       getDetails();
+      getChartData();
 });
-
   $scope.chartViewFunction = function (n) {
     $scope.chartView=n;
   };
+  function getChartData() {
+    var promise = chartDataService.gethistoricalData($scope.ticker,$scope.oneYearAgoDate,$scope.todayDate);
+    promise.then(function(data){
+      $scope.data = JSON.parse(data)
+        .map(function(series) {
+              series.values = series.values.map(function(d) { return {x: d[0], y: d[1] }; });
+              return series;
+          });
+    });
+  }
   function getPrice() {
     var promise=stockDataService.getPriceData($scope.ticker);
     promise.then(function(data){
       $scope.priceData=data;
-      console.log(data);
+      //console.log(data);
     });
   }
   function getDetails(){
     var promise=stockDataService.getDetailsData($scope.ticker);
     promise.then(function(data){
       $scope.detailsData=data;
-      console.log(data);
+      //console.log(data);
     });
   }
-}]);
+    var marginBottom= ($window.innerWidth/100)*10;
+    $scope.options = {
+            chart: {
+                type: 'linePlusBarChart',
+                margin: {
+                    top: 15,
+                    right: 40,
+                    bottom: marginBottom,
+                    left: 70
+                },
+                bars: {
+                    forceY: [0]
+                },
+                bars2: {
+                    forceY: [0]
+                },
+                color: ['#2ca02c', 'darkred'],
+                x: function(d,i) { return i; },
+                xAxis: {
+                    axisLabel: 'X Axis',
+                    tickFormat: function(d) {
+                        var dx = $scope.data[0].values[d] && $scope.data[0].values[d].x || 0;
+                        if (dx > 0) {
+                            return d3.time.format('%b %d')(new Date(dx));
+                        }
+                        return null;
+                    }
+                },
+                tooltips:false,
+                showlegend:false,
+                useVoronoi:false,
+                x2Axis: {
+                    tickFormat: function(d) {
+                        var dx = $scope.data[0].values[d] && $scope.data[0].values[d].x || 0;
+                        return d3.time.format('%b %Y')(new Date(dx));
+                    },
+                    showMaxMin: false
+                },
+                y1Axis: {
+                    axisLabel: 'Y1 Axis',
+                    tickFormat: function(d){
+                        return d3.format(',f')(d);
+                    },
+                    axisLabelDistance: 12
+                },
+                y2Axis: {
+                    axisLabel: 'Y2 Axis',
+                    tickFormat: function(d) {
+                        return '$' + d3.format('s')(d);
+                    }
+                },
+                y3Axis: {
+                    tickFormat: function(d){
+                        return d3.format(',.2s')(d);
+                    }
+                },
+                y4Axis: {
+                    tickFormat: function(d) {
+                        return '$' + d3.format(',.2s')(d);
+                    }
+                }
+            }
+        };
+  }]);
--- www/js/services.js
+++ www/js/services.js
@@ -1,13 +1,38 @@
 angular.module('stock.services',[])
+.factory('encodeURIService', function() {
+  return {
+    encode: function(string) {
+      return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[!'()]/g, escape);
+    }
+  };
+})
+.factory('dateService', function($filter) {
 
-.factory('stockDataService',function ($q,$http) {
+  var currentDate = function() {
+    var d = new Date();
+    var date = $filter('date')(d, 'yyyy-MM-dd');
+    return date;
+  };
+
+  var oneYearAgoDate = function() {
+    var d = new Date(new Date().setDate(new Date().getDate() - 365));
+    var date = $filter('date')(d, 'yyyy-MM-dd');
+    return date;
+  };
+
+  return {
+    currentDate: currentDate,
+    oneYearAgoDate: oneYearAgoDate
+  };
+})
+.factory('stockDataService',function ($q,$http,encodeURIService) {
   var getDetailsData = function(ticker) {
     var deferred = $q.defer(),
-    url ="http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22"+ ticker +"%22)&format=json&env=http://datatables.org/alltables.env";
-
+   query = 'select * from yahoo.finance.quotes where symbol IN ("' + ticker + '")',
+   url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
     $http.get(url)
     .success(function(json){
-      //console.log(json);
+      console.log(json);
       jsonData= json.query.results.quote;
       deferred.resolve(jsonData);
     })
@@ -19,10 +44,10 @@ angular.module('stock.services',[])
   };
   var getPriceData = function(ticker) {
     var deferred = $q.defer(),
-    url= "http://finance.yahoo.com/webservice/v1/symbols/" + ticker + "/quote?format=json&view=detail";
-
+    url = "http://finance.yahoo.com/webservice/v1/symbols/" + ticker + "/quote?format=json&view=detail";
     $http.get(url)
-      .success(function(json){
+        .success(function(json){
+          console.log(json);
       var jsonData= json.list.resources[0].resource.fields;
       deferred.resolve(jsonData);
     })
@@ -35,5 +60,54 @@ angular.module('stock.services',[])
 return{
   getPriceData: getPriceData,
   getDetailsData: getDetailsData
+};
+})
+.factory('chartDataService', function($q,$http, encodeURIService) {
+
+var gethistoricalData= function(ticker, fromDate, todayDate){
+  var deferred = $q.defer(),
+      query = 'select * from yahoo.finance.historicaldata where symbol = "' + ticker + '" and startDate = "' + fromDate + '" and endDate = "' + todayDate + '"';
+      url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
+      $http.get(url)
+      .success(function(json){
+      console.log(json);
+      var jsonData= json.query.results.quote;
+      var priceData=[],
+      volumeData=[];
+      jsonData.forEach(function(datadate){
+        var toMillis = datadate.Date;
+        //console.log(toMillis);
+        date= Date.parse(toMillis);
+          //console.log(date);
+         price= parseFloat(Math.round(datadate.Close*100)/100).toFixed(3);
+        //console.log(price);
+        volume = datadate.Volume;
+        var tempVolume = '[' + date + ',' + volume + ']',
+        tempPrice = '[' + date + ',' + price + ']';
+        //console.log(tempVolume, tempPrice);
+        volumeData.unshift(tempVolume);
+        priceData.unshift(tempPrice);
+      });
+        var finalData =
+        '[{' +
+            '"key":' + '" volume ",'+
+            '"bar":' + ' true, ' +
+            '"values":' + '[' + volumeData + ']' +
+          '},' +
+          '{' +
+              '"key":' + '" '+ ticker +' ",' +
+              '"values":' + '[' + priceData + ']' +
+            '}]';
+
+      deferred.resolve(finalData);
+    })
+    .error(function(error){
+      console.log("Chart error" + error);
+      deferred.reject();
+    });
+    return deferred.promise;
+};
+return{
+  gethistoricalData:gethistoricalData
 };
 });
